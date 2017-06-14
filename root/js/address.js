b498619af6f3ec230d1adf22aec10154375f5e72
keepfriend <szuljf@163.com>
2017-06-15 06:54:44 +0800

    editrootfile

--- root/js/address.js
+++ root/js/address.js
@@ -1,11 +1,1 @@
-$(document).ready(function() {							
-		$(".new-option-r").click(function() {
-			$(this).parent('.user-addresslist').addClass("defaultAddr").siblings().removeClass("defaultAddr");
-		});
-		
-		var $ww = $(window).width();
-		if($ww>640) {
-			$("#doc-modal-1").removeClass("am-modal am-modal-no-btn")
-		}
-		
-	})
+$(document).ready(function(){$(".new-option-r").click(function(){$(this).parent('.user-addresslist').addClass("defaultAddr").siblings().removeClass("defaultAddr")});var $ww=$(window).width();if($ww>640){$("#doc-modal-1").removeClass("am-modal am-modal-no-btn")}})
--- root/testmysql.php
+++
@@ -1,7 +0,0 @@
-<?php 
-$link = mysqli_connect('localhost','root','654321'); 
-if (!$link) { 
-	die('Could not connect to MySQL: ' . mysql_error()); 
-} 
-echo 'Connection OK'; mysqli_close($link); 
-?> 
